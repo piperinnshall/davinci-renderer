596282c13e953c7a0dd38261ee31563d85a867ed
Piper Inns Hall <piper.innshall@gmail.com>
2026-07-04 00:46:08 +1200

    chore: Full sobel shader demo

--- scripts/free_cam.gd
+++ scripts/free_cam.gd
@@ -5,18 +5,34 @@ extends Camera3D
 
 var yaw := 0.0
 var pitch := 0.0
+var base_rotation := Vector3.ZERO
 
 func _ready():
+	base_rotation = rotation
+	get_window().focus_entered.connect(_on_focus)
 	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
 
-func _unhandled_input(e):
-	if e is InputEventMouseMotion:
+func _on_focus():
+	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+
+func _input(e):
+	if e is InputEventKey and e.pressed and e.keycode == KEY_ESCAPE:
+		Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
+
+	if e is InputEventMouseMotion and Input.get_mouse_mode() == Input.MOUSE_MODE_CAPTURED:
 		yaw -= e.relative.x * sens
 		pitch -= e.relative.y * sens
 		pitch = clamp(pitch, deg_to_rad(-89), deg_to_rad(89))
-		rotation = Vector3(pitch, yaw, 0)
 
 func _process(delta):
+	if Input.get_mouse_mode() != Input.MOUSE_MODE_CAPTURED:
+		return
+
+	var rot := base_rotation
+	rot.y += yaw
+	rot.x += pitch
+	rotation = rot
+
 	var move := Vector3.ZERO
 
 	if Input.is_key_pressed(KEY_W):
